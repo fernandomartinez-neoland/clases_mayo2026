3c4f69b3cf2e7dc28c5fdcaa2943fdb2a8c279f1
altrios <famartinez.ti@gmail.com>
2026-05-29 10:11:21 +0200

    polimorfismo y herencia

--- clases.js
+++ clases.js
@@ -1,9 +1,42 @@
-export class clase{
-    // dentro de una clase las variables no necesitan ser declaradas con "const" ni "let"
-    primerNombre
-    // es quien recibe los parametros que se envian hacia la clase
-    constructor (nombre){
-        this.primerNombre=nombre
-        console.log("hola ", this.primerNombre)
-    }
+export class clase {
+  // dentro de una clase las variables no necesitan ser declaradas con "const" ni "let"
+  primerNombre;
+  // es quien recibe los parametros que se envian hacia la clase
+  constructor(nombre) {
+    this.primerNombre = nombre;
+    console.log("hola ", this.primerNombre);
+  }
+
+  despedida(nombre) {
+    console.log(`adios ${nombre}`);
+  }
+}
+
+// HERENCIA
+class padre {
+  casa() {
+    console.log("Tengo una casa");
+  }
+}
+// la calse hijo es una extension del padre, lo cual le da acceso a los metodos del padre
+export class hijo extends padre {}
+
+// POLIMORFISMO
+
+export class animal {
+  hacerruido() {
+    console.log("gruñe");
+  }
+}
+
+export class perro extends animal {
+  hacerruido() {
+    console.log("ladra");
+  }
+}
+
+export class gato extends animal {
+  hacerruido() {
+    console.log("maulla");
+  }
 }
--- modulo.js
+++ modulo.js
@@ -1,3 +1,21 @@
-import {clase} from './clases.js';
+import { clase, hijo, animal, gato, perro } from "./clases.js";
 
-const consola= new clase("fernando");
+const consola = new clase("fernando");
+
+consola.despedida("Bejamin");
+
+// herencia: la clase hijo tiene acceso a los metodos del padre
+const heredero = new hijo();
+
+heredero.casa();
+
+// POLIMORFISMO: aunque las clases hijos comparten el mismo metodo, cada uno actua a su manera
+
+const animales = new animal();
+const perrito = new perro();
+const michi = new gato();
+
+// esto se hace cuando queremos ejecutar varias clases en un solo ciclo, basicamente se le da el mismo nombre de metodo a todas las clases y dentro de un ciclo, con un arreglo que contenga el nombre de las clases y un foreach que los recorra, se ejecutan nombrando al mismo metodos
+animales.hacerruido();
+perrito.hacerruido();
+michi.hacerruido();
